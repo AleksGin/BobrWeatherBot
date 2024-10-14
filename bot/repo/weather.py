deda08526a83ebc082ac190b71a0e5480bf35306
AleksGin <alexanderginin@icloud.com>
2024-10-14 16:27:21 +0500

    create WeatherRepo for API requests

---
+++ bot/repo/weather.py
@@ -0,0 +1,20 @@
+from aiohttp import ClientSession
+from core.schemas import WeatherResponseBase
+
+
+class WeatherRepo:
+    def __init__(self, http_session: ClientSession, api_key: str, url: str) -> None:
+        self.http_session = http_session
+        self.api_key = api_key
+        self.url = url
+
+    async def get_weather_info_by_id(self, city_name: str):
+        params = {
+            "q": city_name,
+            "appid": self.api_key,
+        }
+
+        url = self.url + f"?q={city_name}&appid={self.api_key}&units=metric"
+
+        async with self.http_session.get(url=url, params=params) as resp:
+            return WeatherResponseBase(**await resp.json())
