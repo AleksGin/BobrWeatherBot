962f092b6aa86d8e29101cb05ce6e580f9bf6d6d
AleksGin <alexanderginin@icloud.com>
2024-10-14 21:31:35 +0500

    add try/except to get_info_about_city method + create protected methods

--- bot/services/weather_service.py
+++ bot/services/weather_service.py
@@ -1,8 +1,13 @@
 import logging
-from core.texts import Phrases
-from core.schemas import WeatherResponseBase, Main
-from repo.weather import WeatherRepo
+
 from aiogram.types import Message
+from core.schemas import WeatherResponseBase
+from core.texts import (
+    Phrases,
+    WeatherIcon,
+)
+from pydantic import ValidationError
+from repo.weather import WeatherRepo
 
 
 class WeatherService:
@@ -13,21 +18,44 @@ class WeatherService:
         return await welcome_text.answer(Phrases.welcome_text)
 
     async def get_info_about_city(self, name_of_city: str) -> str:
-        get_weather_info_model = await self.weather_repo.get_weather_info_by_id(
-            city_name=name_of_city
-        )
-        return await self.__prepare_text(weather_model=get_weather_info_model)
+        try:
+            get_weather_info_model = await self.weather_repo.get_weather_info_by_id(
+                city_name=name_of_city
+            )
+            return await self.__prepare_text(weather_model=get_weather_info_model)
+        except ValidationError:
+            return Phrases.validation_error.format(name_of_city)
 
     async def __prepare_text(
         self,
         weather_model: WeatherResponseBase,
         chat_id: int | None = None,
     ) -> str:
-        return await self.__text_for_weather(
-            temp=weather_model.main.temp,
+        weather = weather_model.weather[0]
+        return self.__text_for_weather(
             city=weather_model.name,
+            temp=weather_model.main.temp,
             feels_like=weather_model.main.feels_like,
+            weather_description=weather.description,
+            weather_type_id=weather.id,
+            wind=weather_model.wind.speed,
         )
 
-    async def __text_for_weather(self, temp: float, city: str, feels_like: float) -> str:
-        return f"Погода в {city} {temp}, ощуещается как {feels_like}"
+    def __text_for_weather(
+        self,
+        city: str,
+        temp: float,
+        feels_like: float,
+        weather_description: str,
+        weather_type_id: int,
+        wind: float,
+    ) -> str:
+        weather_icon = WeatherIcon.weather_icon.get(weather_type_id)
+        return Phrases.information_form.format(
+            city,
+            int(temp),
+            int(feels_like),
+            weather_icon,
+            weather_description.capitalize(),
+            int(wind),
+        )
