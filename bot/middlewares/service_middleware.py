efa76894831133729286105eb71aaceecd3d7865
AleksGin <alexanderginin@icloud.com>
2024-10-14 23:31:57 +0500

    add CacheRepository to RepoMiddleware

--- bot/middlewares/service_middleware.py
+++ bot/middlewares/service_middleware.py
@@ -1,20 +1,29 @@
-from typing import Any, Awaitable, Callable
+from typing import (
+    Any,
+    Awaitable,
+    Callable,
+)
 
 from aiogram import BaseMiddleware
 from aiogram.types import TelegramObject
 
-from repo.weather import WeatherRepo
+from repository import (
+    WeatherRepository,
+    CacheRepository,
+)
 from services import WeatherService
 
 
 class RepoMiddleware(BaseMiddleware):
     def __init__(
         self,
-        weather_repo: WeatherRepo,
+        weather_repo: WeatherRepository,
         weather_service: WeatherService,
+        cache_repo: CacheRepository,
     ) -> None:
         self.weather_repo = weather_repo
         self.weather_service = weather_service
+        self.cache_repo = CacheRepository
 
     async def __call__(
         self,
@@ -24,5 +33,6 @@ class RepoMiddleware(BaseMiddleware):
     ) -> Any:
         data["weather_repo"] = self.weather_repo
         data["weather_service"] = self.weather_service
+        data["cache_repo"] = self.cache_repo
 
         return await handler(event, data)
