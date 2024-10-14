415a88282791c31bf1559f42a457a43c64b7dd68
AleksGin <alexanderginin@icloud.com>
2024-10-14 21:29:05 +0500

    add handlers to get info about weather in specific city

--- bot/handlers/handlers.py
+++ bot/handlers/handlers.py
@@ -1,15 +1,28 @@
-from aiogram import Router, F
-from aiogram.filters import CommandStart, Command
+from aiogram import F, Router
+from aiogram.filters import CommandStart
 from aiogram.types import Message
-from core.texts import Phrases
+from services import WeatherService
 
 router = Router()
 
-@router.message(CommandStart)
-async def start_func(message: Message):
-    return await message.answer(Phrases.welcome_text)
 
-@router.message(F.text.capitilize())
-async def get_weather_for_city(message: Message):
-    pass
+@router.message(CommandStart())
+async def start_func(
+    message: Message,
+    weather_service: WeatherService,
+) -> Message:
+    return await weather_service.welcome_text(welcome_text=message)
 
+
+@router.message(F.text)
+async def get_weather_for_city(
+    message: Message,
+    weather_service: WeatherService,
+) -> Message:
+    if not message.text:
+        return await message.answer("Введите название города")
+
+    info = await weather_service.get_info_about_city(
+        name_of_city=message.text.capitalize(),
+    )
+    return await message.answer(info)
