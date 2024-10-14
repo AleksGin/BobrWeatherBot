9badf62ea54955dd3a6cec4de9a818082b0ed4a4
AleksGin <alexanderginin@icloud.com>
2024-10-14 23:30:38 +0500

    add set and get methods to CacheRepository

--- bot/repository/cache.py
+++ bot/repository/cache.py
@@ -1,6 +1,16 @@
-from redis.asyncio import ConnectionPool
+from redis.asyncio import ConnectionPool, Redis
 
 
 class CacheRepository:
     def __init__(self, pool: ConnectionPool) -> None:
+        self.pool = pool
         
+        
+    async def set_weather_info(self, city_name: str, info: str, expire_time: int = 300) -> None:
+        async with Redis.from_pool(connection_pool=self.pool) as redis:
+            await redis.set(name=city_name, value=info, ex=expire_time)
+    
+    async def get_weather_info(self, city_name: str):
+        async with Redis.from_pool(connection_pool=self.pool) as redis:
+            info = await redis.get(name=city_name)
+            return info.decode("utf-8")
